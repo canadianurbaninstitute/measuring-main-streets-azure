0f6df0a6b5a0b819916fb4f6cc482921935f6b40
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2024-01-16 11:29:14 -0500

    base

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/measuring-main-streets" : "",
+			// base: process.env.NODE_ENV === "production" ? "/measuring-main-streets" : "",
+			base: "/measuring-main-streets",
 			},
 	}
 
