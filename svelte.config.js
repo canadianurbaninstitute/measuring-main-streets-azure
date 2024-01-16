e8fe62f2baafb759c2833f6ff25aab8a02975977
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2024-01-16 10:33:03 -0500

    action test

--- svelte.config.js
+++ svelte.config.js
@@ -6,16 +6,22 @@ import {
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// preprocess: vitePreprocess(),
+
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+	// 	// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+	// 	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+	// 	adapter: adapter(),
+	// 	paths: {
+	// 		base: process.env.NODE_ENV === "production" ? "/measuring-main-streets" : "",
+	// 		},
+	// }
+
 	preprocess: vitePreprocess(),
-	
+
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === "production" ? "/measuring-main-streets" : "",
-			},
+	 adapter: adapter(),
 	}
 };
 
