df580a2069ad999ab96b8a947bb7e228641caf1d
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2024-01-24 16:53:30 -0500

    oush

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			// base: process.env.NODE_ENV === "production" ? "/measuring-main-streets" : "",
-			base: "/measuring-main-streets"
+			// base: "/measuring-main-streets"
 		},
 	}
 };
