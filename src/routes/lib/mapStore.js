e38db3d9b0bca5f498de2714540f0e11ea780323
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2024-01-25 15:23:28 -0500

    remove store

--- src/routes/lib/mapStore.js
+++ src/routes/lib/mapStore.js
@@ -2,4 +2,3 @@ import { writable } from 'svelte/store';
 
 export const visitorMapStore = writable(null);
 export const mapStoreList = writable({});
-export const weightMaxStore = writable(0); // Initialize the store with an initial value of 0
