f0f30f671793fb8357f832fc70747d2c5f0de52a
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2024-02-08 13:39:10 -0500

    splash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,43 @@
 <script>
     import './styles.css'
+    import HeroCard from './lib/ui/HeroCard.svelte';
 </script>
 
 <div class="hero">
     <h1> Measuring Main Streets Demo </h1>
 </div>
 
+<div class="card-grid">
+
+<HeroCard
+link={"/map"}
+cardImage={'https://i.imgur.com/DXGEpQB.png'}
+title={'National Map'}
+subtitle={'Explore the national map'}
+/>
+
+<HeroCard
+link={"/casestudies"}
+cardImage={'https://i.imgur.com/DXGEpQB.png'}
+title={'Case Studies'}
+subtitle={'Explore the case studies'}
+/>
+
+</div>
 
 <style>
 
     h1 {
         padding-top: 1em;
     }
+
+    .card-grid {
+		display: grid;
+		grid-gap: 1px;
+		margin: 1em;
+		align-items: center;
+		justify-content: center;
+		grid-template-columns: repeat(1, 1fr);
+	}
 
 </style>
---
+++ src/routes/lib/ui/HeroCard.svelte
@@ -0,0 +1,66 @@
+<script>
+    export let link;
+    export let cardImage;
+    export let title;
+    export let subtitle;
+    
+</script>
+
+
+<a href={link}>
+<div class="card">
+    <div class="card-img-container">
+        <img src={cardImage} alt="card">
+    </div>
+    <div class="card-text-container">
+    <h2>{title}</h2>
+    <h5>{subtitle}</h5>
+    </div>
+</div>
+</a>
+
+
+<style>
+     .card {
+        margin: 1em;
+        background-color: #fff;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid #ddd;
+        border-radius: 1em;
+    }
+
+    .card:hover {
+        cursor: pointer;
+    }
+
+    .card-text-container {
+        border-top: 1px solid #ddd;
+        width: 100%;
+        padding: 1em;
+    }
+    
+    h2 {
+        color: var(--brandDarkBlue);
+        font-weight: 400;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .card-img-container {
+        width: 100%;
+        height: 100px;
+        border-radius: 1em 1em 0 0; /*fix*/
+        overflow: hidden;
+
+    }
+
+    .card-img-container img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        }
+
+</style>
