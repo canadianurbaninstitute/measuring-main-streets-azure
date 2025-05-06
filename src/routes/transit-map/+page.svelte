8bc2b010028e6d1d4374b9610ed60ad18aedd190
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2025-05-06 09:07:26 -0400

    new map

--- src/routes/transit-map/+page.svelte
+++ src/routes/transit-map/+page.svelte
@@ -10,7 +10,7 @@
 
 	import Footer from '../lib/Footer.svelte';
 
-	import stationData from '../lib/data/stations.json';
+	import stationData from '../lib/data/stationsTemp.json';
 
 	mapboxgl.accessToken =
 		'pk.eyJ1IjoiY2FuYWRpYW51cmJhbmluc3RpdHV0ZSIsImEiOiJjbG95bzJiMG4wNW5mMmlzMjkxOW5lM241In0.o8ZurilZ00tGHXFV-gLSag';
@@ -24,12 +24,12 @@
 
 	let selectedStation = {};
 	let stationSelected = false;
+	let lineIds = [];
 
 	let stationAll = stationData.map((item) => {
 		return {
-			label: item.stop_label + ' [' + item.line_label + ']',
+			label: item.stop_label + ' [' + item.line_display_names + ']',
 			value: item.id,
-			group: item.line_label
 		};
 	});
 
@@ -204,10 +204,10 @@
 	];
 
 	function updateStationData(id) {
-
 		selectedStation = stationData.find((station) => station.id === id);
-
-		console.log(selectedStation);
+		
+		// Parse the line_ids string into an array
+		lineIds = selectedStation.line_ids ? selectedStation.line_ids.split(', ') : [];
 
 		civic =
 			selectedStation.arts_and_culture +
@@ -221,8 +221,6 @@
 			{ label: '20-64', value: selectedStation.age_20_64, y: '⠀' },
 			{ label: '65+', value: selectedStation.age_65_over, y: '⠀' }
 		];
-
-		console.log(ageData);
 
 		mobilityData = [
 			{ label: 'Car', value: selectedStation.mobility_car, y: '⠀' },
@@ -250,12 +248,10 @@
 			{ label: 'Owner', value: selectedStation.owners, y: '⠀' },
 			{ label: 'Renter', value: selectedStation.renters, y: '⠀' }
 		];
-
 	}
 
 	// Extract the station selection logic into a reusable function
 	function handleStationSelection(stationId, coordinates) {
-
 		updateStationData(stationId);
 
 		stationSelected = true;
@@ -305,7 +301,7 @@
 	function handleSelect(event) {
 		if (event.detail) {
 			const stationId = event.detail.value;
-			const selectedStation = stationData.find(station => station.id === stationId);
+			const selectedStation = stationData.find((station) => station.id === stationId);
 			const coordinates = [selectedStation.longitude, selectedStation.latitude];
 			handleStationSelection(stationId, coordinates);
 		} else {
@@ -427,7 +423,7 @@
 					.setHTML(
 						`
                 <span class="label-name">${name}</span>
-            `
+            			`
 					)
 					.addTo(map);
 			}
@@ -443,8 +439,8 @@
 		// 			.setLngLat(coordinates)
 		// 			.setHTML(
 		// 				`
-        //         <span class="label-name">${name}</span>
-        //     `
+		//         <span class="label-name">${name}</span>
+		//     `
 		// 			)
 		// 			.addTo(map);
 		// 	}
@@ -520,13 +516,10 @@
 		map.setPaintProperty('employment-size', 'circle-opacity', 0);
 		map.setPaintProperty('employment-size', 'circle-stroke-opacity', 0);
 
-
-
 		switch (selectedTab) {
 			case 'demographics':
 				break;
 			case 'housing':
-				console.log('housing');
 				break;
 			case 'built-form':
 				map.flyTo({
@@ -552,8 +545,8 @@
 				map.setPaintProperty('msn-highdensity', 'line-opacity', 1);
 				break;
 			case 'employment':
-			map.setPaintProperty('employment-size', 'circle-opacity', 0.8);
-			map.setPaintProperty('employment-size', 'circle-stroke-opacity', 1);
+				map.setPaintProperty('employment-size', 'circle-opacity', 0.8);
+				map.setPaintProperty('employment-size', 'circle-stroke-opacity', 1);
 			default:
 				break;
 		}
@@ -570,8 +563,8 @@
 		href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css"
 		type="text/css"
 	/>
-	<link rel="preconnect" href="https://rsms.me/">
-	<link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 </svelte:head>
 
 <div class="hero">
@@ -599,7 +592,6 @@
 		/>
 	</div>
 	<div id="filter-container">
-
 		<h4>Filter</h4>
 		<div class="filter-group">
 			<h4>Status:</h4>
@@ -659,95 +651,102 @@
 	</div>
 </div>
 <div id="content-container">
-	
-	
 	<div id="sidebar" class:active={stationSelected}>
 		{#if stationSelected}
-			<h2>{selectedStation.stop_label}</h2>
-			<h4>{selectedStation.line_label}</h4>
+			<div id="station-container">
+				<div>
+				<div id="transit-logos">
+					{#each lineIds as lineId}
+						<img src="/src/routes/lib/assets/transit-logos/{lineId}.svg" alt="Transit line {lineId}" class="transit-logo" />
+					{/each}
+				</div>
+				<h2>{selectedStation.stop_label}</h2>
+				</div>
+				<h4>{selectedStation.line_display_names}</h4>
+				
 
-			<div class="tag-container">
-				{#each selectedStation.status?.split(', ') || [] as status}
-					<div class="tag">{status}</div>
-				{/each}
 
-				{#each selectedStation.technology?.split(', ') || [] as technology}
-					<div class="tag">{technology}</div>
-				{/each}
+				<div id="tag-container">
+					<div class="tag-list">
+						<h5>Status:</h5>
+						{#each selectedStation.status?.split(', ') || [] as status}
+							<div class="tag">{status}</div>
+						{/each}
+					</div>
+					<div class="tag-list">
+						<h5>Technology:</h5>
+						{#each selectedStation.technology?.split(', ') || [] as technology}
+							<div class="tag">{technology}</div>
+						{/each}
+					</div>
+				</div>
 			</div>
-
-			<hr />
 
 			<Tabs.Root
 				orientation="vertical"
 				value="demographics"
-				id="tab-container"
 				onValueChange={(value) => handleTabChange(value)}
 			>
-				<Tabs.List class="tab-container">
+				<Tabs.List class="tab-root">
 					<Tabs.Trigger value="demographics">Demographics</Tabs.Trigger>
 					<Tabs.Trigger value="housing">Housing</Tabs.Trigger>
 					<Tabs.Trigger value="built-form">Built Form</Tabs.Trigger>
-					<Tabs.Trigger value="business">Main Street Business</Tabs.Trigger>
+					<Tabs.Trigger value="business">Business</Tabs.Trigger>
 					<Tabs.Trigger value="civic">Civic Infrastructure</Tabs.Trigger>
 					<Tabs.Trigger value="employment">Employment</Tabs.Trigger>
 				</Tabs.List>
 				<Tabs.Content value="demographics" class="tab-button">
 					<div class="tab-content">
 						<div class="metric-container">
-						<TransitMetric
-						label={'Population'}
-						value={selectedStation.population}
-						icon={'fluent:people-20-filled'}
-						/>
-						<TransitMetric
-						label={'Households'}
-						value={selectedStation.households}
-						icon={'mdi:house'}
-						/>
+							<TransitMetric
+								label={'Population'}
+								value={selectedStation.population}
+								icon={'fluent:people-20-filled'}
+							/>
+							<TransitMetric
+								label={'Households'}
+								value={selectedStation.households}
+								icon={'mdi:house'}
+							/>
 						</div>
 						<TransitMetric
-						label={'Average Employment Income'}
-						value={selectedStation.average_employment_income}
-						icon={'mdi:wallet'}
+							label={'Average Employment Income'}
+							value={selectedStation.average_employment_income}
+							icon={'mdi:wallet'}
 						/>
 						<div class="metric-container">
-						<TransitMetric
-						label={'Visible Minority'}
-						value={selectedStation.visible_minority + "%"}
-						icon={'mdi:people'}
-						/>
-						<TransitMetric
-						label={'Immigrants'}
-						value={selectedStation.immigrants_non_permanent_residents + "%"}
-						icon={'mdi:globe'}
-						/>
-						<TransitMetric
-						label={'Indigenous'}
-						value={selectedStation.indigenous + "%"}
-						icon={'mdi:people'}
-						/>
+							<TransitMetric
+								label={'Visible Minority'}
+								value={selectedStation.visible_minority + '%'}
+								icon={'mdi:people'}
+							/>
+							<TransitMetric
+								label={'Immigrants'}
+								value={selectedStation.immigrants_non_permanent_residents + '%'}
+								icon={'mdi:globe'}
+							/>
+							<TransitMetric
+								label={'Indigenous'}
+								value={selectedStation.indigenous + '%'}
+								icon={'mdi:people'}
+							/>
 						</div>
-						<TransitMetric
-						label={'University Degree'}
-						value={"46%"}
-						icon={'mdi:school'}
-						/>
+						<TransitMetric label={'University Degree'} value={'46%'} icon={'mdi:school'} />
 						<div class="chart-container">
 							<div class="chart">
-							<BarChart
-								colors={['#002a41', '#0098D6', '#db3069']}
-								data={ageData}
-								zKey="label"
-								xKey="value"
-								yKey="y"
-								title="Age"
-								xMax="100"
-								mode="stacked"
-								legend="true"
-								xSuffix="%"
-								padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
-							/>
+								<BarChart
+									colors={['#002a41', '#0098D6', '#db3069']}
+									data={ageData}
+									zKey="label"
+									xKey="value"
+									yKey="y"
+									title="Age"
+									xMax="100"
+									mode="stacked"
+									legend="true"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
+								/>
 							</div>
 						</div>
 					</div>
@@ -755,179 +754,157 @@
 				<Tabs.Content value="housing" class="tab-button">
 					<div class="tab-content">
 						<div class="metric-container">
-						<TransitMetric
-						label={'Total Dwellings'}
-						value={selectedStation.dwellings}
-						icon={'mdi:house'}
-						/>
-						<TransitMetric
-						label={'Average Value'}
-						value={"$700,000"}
-						icon={'mdi:dollar'}
-						/>
-						<TransitMetric
-						label={'Average Rent'}
-						value={"$2,300"}
-						icon={'mdi:dollar'}
-						/>
+							<TransitMetric
+								label={'Total Dwellings'}
+								value={selectedStation.dwellings}
+								icon={'mdi:house'}
+							/>
+							<TransitMetric label={'Average Value'} value={'$700,000'} icon={'mdi:dollar'} />
+							<TransitMetric label={'Average Rent'} value={'$2,300'} icon={'mdi:dollar'} />
 						</div>
 						<div class="chart-container">
 							<div class="chart">
-							<BarChart
-								colors={['#002a41', '#0098D6']}
-								data={ownerData}
-								zKey="label"
-								xKey="value"
-								yKey="y"
-								title="Owners/Renters"
-								xMax="100"
-								mode="stacked"
-								legend="true"
-								xSuffix="%"
-								padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
-							/>
+								<BarChart
+									colors={['#002a41', '#0098D6']}
+									data={ownerData}
+									zKey="label"
+									xKey="value"
+									yKey="y"
+									title="Owners/Renters"
+									xMax="100"
+									mode="stacked"
+									legend="true"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
+								/>
 							</div>
 							<div class="chart">
-							<BarChart
-							colors={['#002a41', '#0098D6', '#F35D00', '#db3069', '#8A4285', '#43B171']}
-							data={dwellingData}
-							zKey="label"
-							xKey="value"
-							yKey="y"
-							title="Dwelling Type"
-							xMax="100"
-							mode="stacked"
-							legend="true"
-							xSuffix="%"
-							padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
-						/>
-						</div>
-						<div class="chart">
-							<BarChart
-								colors={['#002a41']}
-								data={housingData}
-								xKey="value"
-								yKey="label"
-								title="Housing Construction Year"
-								yMax="100"
-								xSuffix="%"
-								padding={{ top: 0, bottom: 20, left: 60, right: 20 }}
-							/>
+								<BarChart
+									colors={['#002a41', '#0098D6', '#F35D00', '#db3069', '#8A4285', '#43B171']}
+									data={dwellingData}
+									zKey="label"
+									xKey="value"
+									yKey="y"
+									title="Dwelling Type"
+									xMax="100"
+									mode="stacked"
+									legend="true"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
+								/>
 							</div>
-
+							<div class="chart">
+								<BarChart
+									colors={['#002a41']}
+									data={housingData}
+									xKey="value"
+									yKey="label"
+									title="Housing Construction Year"
+									yMax="100"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 60, right: 20 }}
+								/>
+							</div>
 						</div>
 					</div>
 				</Tabs.Content>
 				<Tabs.Content value="built-form" class="tab-button">
 					<div class="tab-content">
-						<div class='metric-container'>
-						<TransitMetric
-						label={'Green Space'}
-						value={selectedStation.greenspace + "sq. m"}
-						icon={'mdi:tree'}
-						/>
-						<TransitMetric
-						label={'Population Density'}
-						value={"570"}
-						icon={'mdi:people'}
-						/>
-						<TransitMetric
-						label={'Employment Density'}
-						value={"380"}
-						icon={'mdi:briefcase'}
-						/>
+						<div class="metric-container">
+							<TransitMetric
+								label={'Green Space'}
+								value={selectedStation.greenspace + 'sq. m'}
+								icon={'mdi:tree'}
+							/>
+							<TransitMetric label={'Population Density'} value={'570'} icon={'mdi:people'} />
+							<TransitMetric label={'Employment Density'} value={'380'} icon={'mdi:briefcase'} />
 						</div>
 						<div class="chart-container">
-						<div class="chart">
-
-						<BarChart
-						colors={['#002a41', '#0098D6', '#db3069']}
-						data={mobilityData}
-						zKey="label"
-						xKey="value"
-						yKey="y"
-						title="Mobility"
-						xMax="100"
-						mode="stacked"
-						legend="true"
-						xSuffix="%"
-						padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
-						/>
-						</div>
+							<div class="chart">
+								<BarChart
+									colors={['#002a41', '#0098D6', '#db3069']}
+									data={mobilityData}
+									zKey="label"
+									xKey="value"
+									yKey="y"
+									title="Mobility"
+									xMax="100"
+									mode="stacked"
+									legend="true"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
+								/>
+							</div>
 						</div>
 						<div />
 					</div></Tabs.Content
 				>
 				<Tabs.Content value="business" class="tab-button">
 					<div class="tab-content">
-						<div class='metric-container'>
+						<div class="metric-container">
 							<TransitMetric
-							label={'Main Street Businesses'}
-							value={selectedStation.total_businesses}
-							icon={'mdi:shop'}
+								label={'Main Street Businesses'}
+								value={selectedStation.total_businesses}
+								icon={'mdi:shop'}
 							/>
 							<TransitMetric
-							label={'Independent Business Index'}
-							value={"0.87"}
-							icon={'mdi:score'}
+								label={'Independent Business Index'}
+								value={'0.87'}
+								icon={'mdi:score'}
 							/>
 						</div>
 						<div class="chart-container">
-						<div class="chart">
-						<BarChart
-								colors={['#43b171', '#F13737', '#2a5cac']}
-								data={businessData}
-								zKey="label"
-								xKey="value"
-								yKey="y"
-								title="Main Street Business"
-								xMax="100"
-								mode="stacked"
-								legend="true"
-								xSuffix="%"
-								padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
-							/>	
+							<div class="chart">
+								<BarChart
+									colors={['#43b171', '#F13737', '#2a5cac']}
+									data={businessData}
+									zKey="label"
+									xKey="value"
+									yKey="y"
+									title="Main Street Business"
+									xMax="100"
+									mode="stacked"
+									legend="true"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
+								/>
+							</div>
 						</div>
-					</div>
 					</div>
 				</Tabs.Content>
 				<Tabs.Content value="civic" class="tab-button">
 					<div class="tab-content">
 						<TransitMetric
 							label={'Civic Infrastructure Loations'}
-							value={"500"}
+							value={'500'}
 							icon={'mdi:museum'}
-							/>
-							<div class="chart-container">
-								<div class="chart">
+						/>
+						<div class="chart-container">
+							<div class="chart">
 								<BarChart
-										colors={['#DB3069', '#F45D01', '#8A4285', '#33AED7', '#43B171']}
-										data={civicData}
-										zKey="label"
-										xKey="value"
-										yKey="y"
-										title="Civic Infrastructure"
-										xMax="100"
-										mode="stacked"
-										legend="true"
-										xSuffix="%"
-										padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
-									/>	
-								</div>
+									colors={['#DB3069', '#F45D01', '#8A4285', '#33AED7', '#43B171']}
+									data={civicData}
+									zKey="label"
+									xKey="value"
+									yKey="y"
+									title="Civic Infrastructure"
+									xMax="100"
+									mode="stacked"
+									legend="true"
+									xSuffix="%"
+									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
+								/>
 							</div>
+						</div>
 					</div>
 				</Tabs.Content>
 				<Tabs.Content value="employment" class="tab-button">
 					<div class="tab-content">
-						<TransitMetric
-							label={'Total Employment'}
-							value={"15,345"}
-							icon={'mdi:briefcase'}
-							/>
-							<div class="chart-container">
-								<div class="chart">
+						<TransitMetric label={'Total Employment'} value={'15,345'} icon={'mdi:briefcase'} />
+						<div class="chart-container">
+							<div class="chart">
 								<BarChart
-									colors={['#db3069', '#0098D6', '#b0b0b0' ]}
+									colors={['#db3069', '#0098D6', '#b0b0b0']}
 									data={employmentData}
 									zKey="label"
 									xKey="value"
@@ -939,8 +916,8 @@
 									xSuffix="%"
 									padding={{ top: 0, bottom: 20, left: 0, right: 20 }}
 								/>
-								</div>
 							</div>
+						</div>
 					</div>
 				</Tabs.Content>
 			</Tabs.Root>
@@ -991,13 +968,11 @@
 		flex-direction: column;
 	}
 
-
 	#content-container {
 		display: flex;
 		flex-direction: column;
 		overflow: hidden;
 	}
-
 
 	.select-wrapper {
 		width: 100%;
@@ -1008,7 +983,6 @@
 		width: 100%;
 		display: flex;
 		flex-direction: column;
-		padding: 1em;
 		overflow-y: scroll;
 		overflow-x: hidden;
 		border-top: 1px solid #eee;
@@ -1023,10 +997,23 @@
 		width: 100%;
 	}
 
+	#station-container {
+		border-bottom: 1px solid #eee;
+		padding: 1em;
+		margin: 0 0 1em 0;
+	}
+
 	h2,
-	h4 {
+	h4,
+	h5 {
 		margin: 0;
 		padding: 0.2em 0 0.2em 0;
+	}
+
+	h2 {
+		font-weight: 600;
+		text-transform: uppercase;
+		font-family: 'Inter', sans-serif;
 	}
 
 	.tag {
@@ -1035,10 +1022,12 @@
 		padding: 0.5em;
 		border: 2px solid #eee;
 		border-radius: 10em;
+		font-size: 0.8em;
 	}
 
-	.tag-container {
+	.tag-list {
 		display: flex;
+		align-items: center;
 		flex-direction: row;
 		gap: 0.5em;
 		width: 100%;
@@ -1053,7 +1042,7 @@
 		height: 60px;
 		border-bottom: 1px solid #eee;
 		border-top: 1px solid #eee;
-		}
+	}
 
 	.filter-group {
 		display: flex;
@@ -1070,7 +1059,14 @@
 	}
 
 	.tab-content {
-		padding: 1em 0 1em 0;
+		padding: 1em;
+	}
+
+	:global(.tab-root) {
+		padding: 0 1em 0 1em;
+		display: grid;
+		width: 100%;
+		grid-template-columns: 1fr 1fr 1fr; /* Two columns; adjust if more tabs */
 	}
 
 	@media only screen and (min-width: 768px) {
@@ -1113,9 +1109,22 @@
 		}
 	}
 
-	hr {
+	/* hr {
 		border: 0.5px solid #eee;
 		width: 100%;
 		margin: 1em 0 1em 0;
+	} */
+
+	#transit-logos {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 0.5em;
+		margin: 0.5em 0;
+	}
+
+	.transit-logo {
+		height: 30px;
+		width: auto;
+		border-radius: 4px;
 	}
 </style>
