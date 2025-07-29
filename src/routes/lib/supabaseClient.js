60ed801eddc81b263f12db42605f00df81b0fab5
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2025-07-29 17:32:03 -0400

    new fix

--- src/routes/lib/supabaseClient.js
+++ src/routes/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$env/static/private';
 
 export const supabase = createClient(
-    PUBLIC_SUPABASE_URL,
-    PUBLIC_SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
   );
