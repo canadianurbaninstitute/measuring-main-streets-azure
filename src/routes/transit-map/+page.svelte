7a59a14058617c36500095d85eac92666b3f56e8
iw-00 <133152066+iw-00@users.noreply.github.com>
2025-09-17 13:11:18 -0400

    Rearrange built form tab

--- src/routes/transit-map/+page.svelte
+++ src/routes/transit-map/+page.svelte
@@ -922,6 +922,18 @@
 							<div class="tab-content">
 								<div class="metric-container">
 									<TransitMetric
+										label={'Population Density'}
+										value={Math.round(selectedStation.PopulationDensity).toLocaleString() + ' / sq. km'}
+										icon={'mdi:people'}
+									/>
+									<TransitMetric
+										label={'Employment Density'}
+										value={Math.round(selectedStation.EmploymentDensity).toLocaleString() + ' / sq. km'}
+										icon={'mdi:briefcase'}
+									/>
+								</div>
+								<div class="metric-container">
+									<TransitMetric
 										label={'Green Space'}
 										value={Math.round(stationBuiltForm.area_green).toLocaleString() + ' sq. m'}
 										icon={'mdi:pine-tree-variant'}
@@ -940,18 +952,6 @@
 										label={'Parking'}
 										value={Math.round(stationBuiltForm.area_parking).toLocaleString() + ' sq. m'}
 										icon={'mdi:car'}
-									/>
-								</div>
-								<div class="metric-container">
-									<TransitMetric
-										label={'Population Density'}
-										value={Math.round(selectedStation.PopulationDensity).toLocaleString() + ' / sq. km'}
-										icon={'mdi:people'}
-									/>
-									<TransitMetric
-										label={'Employment Density'}
-										value={Math.round(selectedStation.EmploymentDensity).toLocaleString() + ' / sq. km'}
-										icon={'mdi:briefcase'}
 									/>
 								</div>
 								<div></div>
